14348097a2f5e5b42c3e1dd6816f3f2c30ae3b16
Eragon189 <144192904+Eragon189@users.noreply.github.com>
2024-05-15 20:19:38 -0700

    Update compresor.js

--- compresor.js
+++ compresor.js
@@ -1,1 +1,22 @@
-
+let outputFile = null;
+   
+    function caculate() {
+      //var to store origenal code plus tags(AKA taged)
+      let tagedJs = null; 
+    let tagedCss = null;
+      let tagedHtml = null;
+      //geing html,css, and javascript code
+      let html = getElementById("html")
+        let css = getElementById("css")
+        let js = getElementById("js")
+			//aplyes tags to html,css, and javascript
+  tagedCss = "\<style\><br>" + css + "<br>\<style\>"
+  tagedJs = "\<script\><br>" + css + "<br>\<script\>"
+			//removes </html> tag
+	tagedHtml = html - "\</html\>"
+  
+      
+      outputFile = html + "\<style\><br>" + css +"\</style\><br>\<script\><br>" + js + "\</script\><br>\</html\>"
+    }    
+	  caculate()
+getElementById("output").innerhtml=(outputFile)
